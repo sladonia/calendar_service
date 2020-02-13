b1a19ad6aa47dbc4dc1bf96695f6c898edf78648
Serhii Ladonia <ladonya.s@gmail.com>
2020-02-13 11:34:16 +0200

    add logger

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module calendar_service
 go 1.13
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/configor v1.1.1
 	github.com/jinzhu/gorm v1.9.12
 	github.com/lib/pq v1.1.1
 	github.com/stretchr/testify v1.4.0
+	go.uber.org/zap v1.13.0
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"calendar_service/src/config"
+	"calendar_service/src/logger"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	if err := config.Load(); err != nil {
 		panic(err)
 	}
+	if err := logger.InitLogger(config.Config.ServiceName, config.Config.LogLevel); err != nil {
+		panic(err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +29,14 @@ func main() {
 	})
 
 	srv := &http.Server{Addr: config.Config.Port, Handler: r}
-	if err := srv.ListenAndServe(); err != nil {
-		panic(err)
-	}
+	logger.Logger.Infof("start listening on port %s", config.Config.Port)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			panic(err)
+		}
+	}()
+
+	<-done
+	logger.Logger.Info("shutting down gracefully")
+	logger.Logger.Sync()
 }
